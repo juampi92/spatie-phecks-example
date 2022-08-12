08cec559f2a0b613eaa5b6abc04687d2955d48fb
Juan Pablo Barreto <juampi92@gmail.com>
2022-08-12 15:38:59 +0200

    install phecks

---
+++ config/phecks.php
@@ -0,0 +1,30 @@
+<?php
+
+use Phecks\Checks;
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Checks
+    |--------------------------------------------------------------------------
+    |
+    | List of Checks that will run on the current repository.
+    | Feel free to group checks in sub-folders.
+    |
+    */
+
+    'checks' => [
+        // Checks\Check::class,
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Baseline
+    |--------------------------------------------------------------------------
+    |
+    | Where the baseline is located.
+    |
+    */
+
+    'baseline' => '.phecks.baseline.json',
+];
